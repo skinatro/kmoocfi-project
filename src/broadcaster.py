af5403079b1e62e606292062776a5636bbaa30dd
Skinatro <work.omkate@gmail.com>
2025-08-12 00:27:34 +0530

    exc 4.9

--- src/broadcaster.py
+++ src/broadcaster.py
@@ -1,24 +1,28 @@
-"""
-Simple NATS to Discord Broadcaster
-"""
 import os
 import asyncio
 from discord_webhook import DiscordWebhook
 from nats import connect
 
+
 NATS_URL = os.environ.get("NATS_URL", "nats://nats-service:4222")
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
+LOG_ONLY = os.environ.get("LOG_ONLY", "false").lower() == "true"
+
 
 def push_to_discord(message: str):
-    """Sends message to discord"""
-    webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, rate_limit_retry=True, content=message)
-    webhook.execute()
+    """Sends message to discord only if LOG_ONLY is False"""
+    if LOG_ONLY:
+        print(f"LOG ONLY MODE: {message}")
+        webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, rate_limit_retry=True, content=message)
+        webhook.execute()
+
 
 async def message_handler(msg):
-    """Get the message and send it to discord"""
+    """Get the message and either log or send to discord"""
     subject = msg.subject
     data = msg.data.decode()
     push_to_discord(f"Received on [{subject}]: {data}")
+
 
 async def main():
     """Listen to subject db-updates and call message_handler"""
@@ -27,6 +31,7 @@ async def main():
     
     while True:
         await asyncio.sleep(1)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
